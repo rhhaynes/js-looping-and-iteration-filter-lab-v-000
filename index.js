a1623d133d0385384c3bdfc130211df8583362d1
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-22 13:48:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function findMatching(){
+function findMatching(arr, str){
+  const newArr = [];
+  for (const elem of arr){
+    if (elem === str) { newArr.push(elem) }
+  }
 }
 
 function fuzzyMatch(){
